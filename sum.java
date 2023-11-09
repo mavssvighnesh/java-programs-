b571b89326d2527b5f043169872c46bf84278099
mavssvighnesh <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-09 22:39:22 +0530

    fixed some issues

--- sum.java
+++ sum.java
@@ -1,5 +1,5 @@
 import java.util.StringTokenizer;
-import java.util.*;
+import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
@@ -9,7 +9,7 @@ public class sum {
 
         StringTokenizer st = new StringTokenizer(input);
         int sum = 0;
-        Close sc;
+        sc.close();
 
         while (st.hasMoreTokens()) {
             int num = Integer.parseInt(st.nextToken());

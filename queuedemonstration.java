a3581f64cad87d542ffd3e03b213d40e4ff36bf0
mavssvighnesh <109013315+mavssvighnesh@users.noreply.github.com>
2023-11-10 20:30:10 +0530

    fixed some issues

---
+++ queuedemonstration.java
@@ -0,0 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class queuedemonstration {
+    private Deque<Object> deque;
+    private int maxSize;
+
+    public queuedemonstration(int maxSize) {
+        this.maxSize = maxSize;
+        deque = new ArrayDeque<>(maxSize);
+    }
+
+    public void enqueue(Object obj) throws QueueFullException {
+        if (deque.size() == maxSize) {
+            throw new QueueFullException("Queue is full");
+        }
+        deque.addLast(obj);
+    }
+
+    public Object dequeue() throws QueueEmptyException {
+        if (deque.isEmpty()) {
+            throw new QueueEmptyException("Queue is empty");
+        }
+        return deque.removeFirst();
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue(5);
+
+        try {
+            queue.enqueue(1);
+            queue.enqueue(2);
+            queue.enqueue(3);
+            queue.enqueue(4);
+            queue.enqueue(5);
+
+            System.out.println(queue.dequeue());
+            System.out.println(queue.dequeue());
+            System.out.println(queue.dequeue());
+            System.out.println(queue.dequeue());
+            System.out.println(queue.dequeue());
+        } catch (QueueFullException e) {
+            System.out.println("Error adding to queue: " + e.getMessage());
+        } catch (QueueEmptyException e) {
+            System.out.println("Error removing from queue: " + e.getMessage());
+        }
+    }
+}
+
